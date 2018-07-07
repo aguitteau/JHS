c448326f8381ed2567f15f3e5cf095d86cb22770
aguitteau <39445709+aguitteau@users.noreply.github.com>
2018-07-06 18:45:19 -0700

    more changes
    expanded constructor and added main method

--- TestCalculator.java
+++ TestCalculator.java
@@ -3,6 +3,10 @@ public class TestCalculator {
     private double value;
     
     public TestCalculator () {
+        value = 0.0;
+    }
+    
+    public static void main (String[] args) { //main method to test calculator
         
     }
     
