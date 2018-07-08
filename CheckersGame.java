4250c395f088da2d9b1007d97f5e27bd8b541234
aguitteau <39445709+aguitteau@users.noreply.github.com>
2018-07-08 15:27:51 -0700

    variety of changes
    -tweaked and completed methods initializeBoard and initializePieces
    -completed displayBoard method, which for each x,y board coordinate displays what is "nearest" to the birds-eye-view "camera"
    -minor fixes

--- CheckersGame.java
+++ CheckersGame.java
@@ -5,29 +5,41 @@ public class CheckersGame {
     public CheckersGame () {
         board = new char[8][8][2];
         initializeBoard();
+        initializePieces();
     }
     
     public void initializeBoard () {
         for (int r = 0; r < board.length; r++) {
-            for (int c = 0; c < board[r].length; c++) {
+            for (int c = 0; c < board[0].length; c++) {
                 
-                if ((r + c) % 2 == 0) {
-                    board[r][c][0] = ' ';
-                } else {
-                    board[r][c][0] = '.';
-                }
+                if ((r + c) % 2 == 0) { board[r][c][0] = ' ';
+                } else {                board[r][c][0] = '.'; }
             }
         }
     }
     
     public void initializePieces () {
-        
+        for (int r = 0; r < board.length; r++) {
+            for (int c = 0; c < board[0].length; c++) {
+                
+                if ((r + c) % 2 != 0) {
+                    if (r < 3)      board[r][c][1] = 'W';
+                    else if (r > 4) board[r][c][1] = 'B'; 
+                } else
+                    board[r][c][1] = ' ';
+            }
+        }
     }
     
     public void displayBoard () {
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[r].length; c++) {
-                System.out.print(board[r][c][0] + " ");
+                
+                if (board[r][c][1] == 'W' || board[r][c][1] == 'B')
+                    System.out.print(board[r][c][1] + " ");
+                else
+                    System.out.print(board[r][c][0] + " ");
+                
             }
             System.out.println();
         }
