4d1071b9dc50bbac692784cb45be25c03de853bd
darthvader1521 <djdebettencourt@gmail.com>
2018-07-07 22:41:35 -0700

    changed square color formula

--- CheckersGame.java
+++ CheckersGame.java
@@ -13,9 +13,12 @@ public class CheckersGame {
         for (int r = 0; r < board.length; r++) {
             for (int c = 0; c < board[r].length; c++) {
                 
-                if (squareColor) { board[r][c][0] = ' '; squareColor = !squareColor;
-                } else {           board[r][c][0] = '·'; squareColor = !squareColor;
+                if ((r + c) % 2 == 0) {
+                    board[r][c][0] = ' ';
+                } else {
+                    board[r][c][0] = '.';
                 }
+                
                 
             }
             
